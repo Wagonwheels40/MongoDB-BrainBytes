df1f8e6c7d736177ea63a172726a81ea9a6b4f8d
Wagonwheels40 <116485787+Wagonwheels40@users.noreply.github.com>
2023-06-08 08:41:06 +1000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://dkwagner40:Lmbas123@cluster0.pfvqylq.mongodb.net/flashcards", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Create a data schema
+const notesSchema = {
+  title: String,
+  content: String,
+};
+
+const Note = mongoose.model("Note", notesSchema);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "/index.html"));
+});
+
+// Add a new route to handle dynamic JavaScript files
+app.get("/js/sets.js", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "sets.js"));
+});
+
+app.get("/js/hide.js", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "hide.js"));
+});
+
+app.get("/js/script.js", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "script.js"));
+});
+
+//app.post
+app.post("/", function (req, res) {
+  let newNote = new Note({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  newNote.save();
+  res.redirect("/");
+});
+
+app.listen(3000, function () {
+  console.log("Server is running on port 3000");
+});
