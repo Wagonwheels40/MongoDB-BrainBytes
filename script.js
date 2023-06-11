810c6efdaf4e8114d86b529d3c10d71f0655049a
Wagonwheels40 <116485787+Wagonwheels40@users.noreply.github.com>
2023-06-11 11:12:26 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
-function flip() {
-    card.classList.toggle('flipped');
-    if (card.classList.contains('flipped')) {
-      frontContent.style.display = 'none';
-      backContent.style.display = 'block';
-    } else {
-      frontContent.style.display = 'block';
-      backContent.style.display = 'none';
-    }
-  }
-  
-  const cardContainer = document.getElementById('card-container');
-  const saveButton = document.getElementById('save_card');
-  
-  saveButton.addEventListener('click', function() {
-    cardContainer.removeAttribute('hidden');
-  });
-  
-  
+document.getElementById("openSet").addEventListener("click", function() {
+  document.querySelector(".input-container").classList.add("hidden");
+  document.querySelector(".flashcard-container").classList.remove("hidden");
+});
+
+const cardList = document.getElementById('card-list');
+const openSetButton = document.getElementById('openSet');
+const inputContainer = document.querySelector('.input-container');
+const flashcardContainer = document.querySelector('.flashcards-container');
+
+function fetchAndDisplayFlashcards() {
+  fetch('/notes')
+    .then((response) => response.json())
+    .then((cards) => {
+      cardList.innerHTML = '';
+      cards.forEach(function(card) {
+        const newCard = document.createElement('li');
+        newCard.innerHTML = `
+          <div class="card">
+            <div class="front">
+              <h3>${card.front}</h3>
+            </div>
+            <div class="back">
+              <h3>${card.back}</h3>
+            </div>
+          </div>
+        `;
+        cardList.appendChild(newCard);
+        newCard.addEventListener('click', function() {
+          newCard.classList.toggle('flipped');
+        });
+      });
+
+      inputContainer.classList.add('hidden');
+      flashcardContainer.classList.remove('hidden');
+    })
+    .catch((error) => {
+      console.log("Error fetching flashcards:", error);
+    });
+}
+
+openSetButton.addEventListener('click', function() {
+  fetchAndDisplayFlashcards();
+});
+
+// Still Not working //
